97ef4821237233ec577338c70fafda3acb2a0930
SDN33 <s.deinegri2@gmail.com>
2025-02-22 18:12:48 +0100

    Accueil en cours

--- app/(app)/(protected)/_layout.tsx
+++ app/(app)/(protected)/_layout.tsx
@@ -1,31 +1,43 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 import { colors } from "@/constants/colors";
 import { useColorScheme } from "@/lib/useColorScheme";
 
 export default function ProtectedLayout() {
-	const { colorScheme } = useColorScheme();
+    const { colorScheme } = useColorScheme();
 
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarStyle: {
-					backgroundColor:
-						colorScheme === "dark"
-							? colors.dark.background
-							: colors.light.background,
-				},
-				tabBarActiveTintColor:
-					colorScheme === "dark"
-						? colors.dark.foreground
-						: colors.light.foreground,
-				tabBarShowLabel: false,
-			}}
-		>
-			<Tabs.Screen name="index" options={{ title: "Home" }} />
-			<Tabs.Screen name="settings" options={{ title: "Settings" }} />
-		</Tabs>
-	);
+    return (
+        <Tabs
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor:
+                        colorScheme === "dark"
+                            ? colors.dark.background
+                            : colors.light.background,
+                },
+                tabBarActiveTintColor:
+                    colorScheme === "dark"
+                        ? colors.dark.foreground
+                        : colors.light.foreground,
+                tabBarShowLabel: false,
+                tabBarIcon: ({ color, size }) => {
+                    let iconName: "home" | "settings" = "home";
+
+                    if (route.name === "index") {
+                        iconName = "home";
+                    } else if (route.name === "settings") {
+                        iconName = "settings";
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+        >
+            <Tabs.Screen name="index" options={{ title: "Accueil" }} />
+            <Tabs.Screen name="settings" options={{ title: "Réglages" }} />
+        </Tabs>
+    );
 }
